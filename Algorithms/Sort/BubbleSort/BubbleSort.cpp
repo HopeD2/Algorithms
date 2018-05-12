fd5108de4d1a71549d484a4ba1dd8971b9edd50e
Lobhas P <lobhaspatil@gmail.com>
2018-05-12 17:56:22 +0530

    Fix bubble sort incorrect implementation

--- Algorithms/Sort/BubbleSort/BubbleSort.cpp
+++ Algorithms/Sort/BubbleSort/BubbleSort.cpp
@@ -9,10 +9,10 @@ void BubbleSort::vSort()
 {
 	for (int i = 0; i < m_sizeOfArray; i++)
 	{
-		for (int j = 0; j < m_sizeOfArray; j++)
+		for (int j = 0; j < m_sizeOfArray - i -1; j++)
 		{
-			if(m_arr[i] < m_arr[j])
-				vSwap(&m_arr[i], &m_arr[j]);
+			if(m_arr[j] > m_arr[j+1])
+				vSwap(&m_arr[j], &m_arr[j+1]);
 		}
 	}
 }
